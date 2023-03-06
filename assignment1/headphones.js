514602353c284bc562126b496a02d39fa467d4fc
rgbrgmnn <rgbrgmnn@iastate.edu>
2023-03-06 13:23:34 -0600

    added data.json and js files

---
+++ assignment1/headphones.js
@@ -0,0 +1,53 @@
+fetch("data.json")
+    .then(response => response.json())
+    .then(data => dataToHTML(data))
+
+    function dataToHTML(data) {
+        let mainContainer = document.getElementById("headphones");
+
+        for (let i = 0; i < data.Headphones.length; i++){
+            let name = data.Headphones[i].productName;
+            let url = data.Headphones[i].product_url;
+            let noiseCancelling = data.Headphones[i].noiseCancelling;
+            let connectionType = data.Headphones[i].connectionType;
+            let waterResistant = data.Headphones[i].waterRessistant;
+            let batteryLife = data.Headphones[i].batterLife;
+            let microphone = data.Headphones[i].microphone;
+            let compatableOS = data.Headphones[i].compatableOS;
+            let image = data.Headphones[i].image_url;
+        
+
+        let mycol = document.createElement("div");
+
+        mycol.innerHTML = `
+        <div class="card shadow-sm">
+            <img src=${image} alt=${name} hspace="30" vspace="30">
+              <div class="card-header">
+                <p class="card-text">${name}</p>
+              </div>
+              <div class="card-body">
+                <p class="card-text">
+                <ul>
+                  <li>Noise Cancelling: ${noiseCancelling}</li>
+                  <li>Connection Type: ${connectionType}</li>
+                  <li>Water Resistant: ${waterResistant}</li>
+                  <li>Battery Life: ${batteryLife}</li>
+                  <li>Built-in Microphone: ${microphone} </li>
+                  <li>Compatable Software: ${compatableOS}</li>
+                </ul>
+                </p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary"
+                      onclick="window.open('${url}');">Buy
+                      Now</button>
+                    <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
+                  </div>
+                  <!-- <small class="text-muted">9 mins</small> -->
+                </div>
+              </div>
+            </div>
+        `;
+        mainContainer.append(mycol);
+        }
+    }
---
+++ assignment1/laptops.js
@@ -0,0 +1,51 @@
+fetch("data.json")
+    .then(response => response.json())
+    .then(data => dataToHTML(data))
+
+    function dataToHTML(data){
+        let mainContainer = document.getElementById("laptops");
+
+        for (let i = 0; i < data.Laptops.length; i++){
+            let name = data.Laptops[i].productName;
+            let url = data.Laptops[i].product_url;
+            let size = data.Laptops[i].productDescription.size;
+            let resolution = data.Laptops[i].productDescription.resolution;
+            let processor = data.Laptops[i].productDescription.processor;
+            let ram = data.Laptops[i].productDescription.ram;
+            let storage = data.Laptops[i].productDescription.storage;
+            let os = data.Laptops[i].productDescription.operatingSystem;
+            let image = data.Laptops[i].image_url;
+
+            let mycol = document.createElement("div");
+
+            mycol.innerHTML = `
+            <div class="card shadow-sm">
+              <img src=${image} alt=${name} hspace="30" vspace="30" >
+              <div class="card-header">
+                <p class="card-text">${name}</p>
+              </div>
+              <div class="card-body">
+                <p class="card-text">
+                <ul>
+                  <li>Size: ${size}</li>
+                  <li>Resolution: ${resolution}</li>
+                  <li>Processor: ${processor}</li>
+                  <li>RAM: ${ram}</li>
+                  <li>Storage: ${storage}</li>
+                  <li>Operating System: ${os}</li>
+                </ul>
+                </p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary"
+                      onclick="window.open('${url}');">Buy
+                      Now</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            `;
+            mainContainer.append(mycol);
+        }
+
+    }
