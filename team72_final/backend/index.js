4e8824ccb9a9898795e68386bcd900823fddb701
rbargmann <125072380+rbargmann@users.noreply.github.com>
2023-05-05 17:41:50 -0500

    Update index.js
    Added post method

--- team72_final/backend/index.js
+++ team72_final/backend/index.js
@@ -38,4 +38,41 @@ app.get("/:name", async (req, resp) => {
   const allProducts = await Product.find(query);
   console.log(allProducts);
   resp.send(allProducts);
+}); 
+
+app.post("/insert", async (req, res) => {
+  console.log(req.body);
+  const p_id = req.body._id;
+  const pname = req.body.productName;
+  const pprice = req.body.price;
+  const psize = req.body.productDescription.size;
+  const pres = req.body.productDescription.resolution;
+  const pproc = req.body.productDescription.processor;
+  const pram = req.body.productDescription.ram;
+  const pstorage = req.body.productDescription.storage;
+  const poper = req.body.productDescription.operatingSystem;
+  const pimage = req.body.image_url;
+
+  const formData = new Product({
+    _id: p_id,
+    productName: pname,
+    productDescription: {
+      size: psize,
+      resolution: pres,
+      processor: pproc,
+      ram: pram,
+      storage: pstorage,
+      operatingSystem: poper
+    },
+    price: pprice,
+    image_url: pimage
+  });
+  try {
+    // await formData.save();
+    await Product.create(formData);
+    const messageResponse = { message: `Product ${p_id} added correctly` };
+    res.send(JSON.stringify(messageResponse));
+  } catch (err) {
+    console.log("Error while adding a new product:" + err);
+  }
 });
